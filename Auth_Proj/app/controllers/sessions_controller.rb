39ad918716902d3b44f252eb3e36f5b641795451
Feifei Yang <feifeiyang@Feifeis-MacBook-Air-2.local>
2021-09-20 13:31:34 -0700

    login

--- Auth_Proj/app/controllers/sessions_controller.rb
+++ Auth_Proj/app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
 
     def new
+        render :new
+
     end 
 
     def create
+       @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
     end 
 
     def destroy
+        
     end 
 end 
--- Auth_Proj/app/models/user.rb
+++ Auth_Proj/app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
 
+
     after_initialize :ensure_session_token
 
     validates :username, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence:true
+    validates :password, length: {minimum: 6, allow_nil: true}
     # Hash collisions - why do we not include unique above?
 
     def self.find_by_credentials(username, password)
@@ -25,7 +27,9 @@ class User < ApplicationRecord
 
     def password=(password)
         # Why BCrypt was not working in Pry
-        self.password_digest = BCrypt::Password.create(password)
+        #com back the gem bcrypt on gem
+        self.password_digest = BCrypt::Password.create(password) 
+        # passsword_digest: "$2a$12$8caRbNzPMqUTy8XLq74Fn.9ekOBtAAiXqNKDEl8LJJGs1FPJhFw6e"
         @password = password
     end
 
