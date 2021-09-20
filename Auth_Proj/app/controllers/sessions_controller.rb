1fb1159abb7ae61ceb9bf349fefddbf992bd2490
gtsimis <gt2186@gmail.com>
2021-09-20 17:23:42 -0400

    Added User Controller

--- Auth_Proj/app/controllers/sessions_controller.rb
+++ Auth_Proj/app/controllers/sessions_controller.rb
@@ -2,11 +2,15 @@ class SessionsController < ApplicationController
 
     def new
         render :new
-
     end 
 
     def create
-       @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+            session[:session_token] = @user.reset_session_token!
+            redirect_to cats_url
+        else
+            redirect_to new_session_url
+        end
     end 
 
     def destroy
